9eb921348933506e17e55b24e88898da6a9129a3
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-09-05 22:21:57 +0100

    change webpack dev server config. lol

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ["*", ".js", ".jsx"],
   },
   devServer: {
-    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebPackPlugin({
